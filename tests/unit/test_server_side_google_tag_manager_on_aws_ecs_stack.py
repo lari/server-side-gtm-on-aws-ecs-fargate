8d6883d8d52e19a51e7083fc7c36ea31d0ef0538
Lari Haataja <lari.haataja@gmail.com>
2023-01-21 23:45:16 +0200

    Initial commit

---
+++ tests/unit/test_server_side_google_tag_manager_on_aws_ecs_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from server_side_google_tag_manager_on_aws_ecs.server_side_google_tag_manager_on_aws_ecs_stack import ServerSideGoogleTagManagerOnAwsEcsStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in server_side_google_tag_manager_on_aws_ecs/server_side_google_tag_manager_on_aws_ecs_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = ServerSideGoogleTagManagerOnAwsEcsStack(app, "server-side-google-tag-manager-on-aws-ecs")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
