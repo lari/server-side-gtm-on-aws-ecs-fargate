1bfd1e20fa86ec38c626f6f8adcad0337e5d2a0d
Lari Haataja <lari.haataja@gmail.com>
2023-01-21 23:58:25 +0200

    Init CDK App

--- app.py
+++ app.py
@@ -8,14 +8,7 @@ from server_side_google_tag_manager_on_aws_ecs.server_side_google_tag_manager_on
 
 app = cdk.App()
 ServerSideGoogleTagManagerOnAwsEcsStack(app, "ServerSideGoogleTagManagerOnAwsEcsStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
-
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+    env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
 
     # Uncomment the next line if you know exactly what Account and Region you
     # want to deploy the stack to. */
